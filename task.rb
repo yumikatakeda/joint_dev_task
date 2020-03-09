7d858a4f1925cb804d4b36297eaebfbc086de6a3
武田　佑美香 <yumikatakeda@takedayumikanoMacBook-Pro.local>
2020-03-09 22:21:37 +0900

    質問1,2,3,4,5に回答

--- task.rb
+++ task.rb
@@ -5,29 +5,31 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names << "斎藤"
+  p names
 end
 
 def q2
   array1 = %w(dog cat fish)
   array2 = %w(bird bat tiger)
 
-  # 以下に回答を記載
-
+  # 以下に回答を記載 
+  array=array1+array2
+  p array 
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  p sports.compact!
 end
 
 def q5
@@ -35,7 +37,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
